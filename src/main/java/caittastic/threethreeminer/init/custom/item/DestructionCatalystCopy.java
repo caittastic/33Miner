c33ec104a2e2ca4a48fce1eee623899e3ed4954b
smoluwu <79610644+smoluwu@users.noreply.github.com>
2022-02-06 02:06:17 +0000

    neatned code, added a check to not break a block under spawn protection, added method to give player items from broken brocks directly if the inventory is not empty

--- src/main/java/caittastic/threethreeminer/init/custom/item/DestructionCatalystCopy.java
+++ src/main/java/caittastic/threethreeminer/init/custom/item/DestructionCatalystCopy.java
@@ -1,6 +1,8 @@
 package caittastic.threethreeminer.init.custom.item;
 
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.UseOnContext;
@@ -8,12 +10,18 @@ import net.minecraft.world.InteractionResult;
 import net.minecraft.core.Direction;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.storage.loot.LootContext;
+import net.minecraftforge.common.ForgeHooks;
+import net.minecraftforge.server.ServerLifecycleHooks;
 
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 public class DestructionCatalystCopy extends Item {
 
@@ -25,7 +33,7 @@ public class DestructionCatalystCopy extends Item {
     public InteractionResult onItemUseFirst(ItemStack stack, UseOnContext context) {
         Level world = context.getLevel();
         context.getPlayer().getDirection();
-        if (!world.isClientSide){
+        if(!world.isClientSide){
             superBreaker(context);
         }
         return super.onItemUseFirst(stack, context);
@@ -37,65 +45,71 @@ public class DestructionCatalystCopy extends Item {
     private void superBreaker(UseOnContext context) {
         Level world = context.getLevel();
         BlockPos blockPos = context.getClickedPos();
-        BlockPos newBlockPos = blockPos;
+        BlockPos newBlockPos;
         int[] iterateWidthHeight = {-1, 0, 1};
-        int[] itterateDepth = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-        int[] negativeItterateDepth = {0, -1, -2, -3, -4, -5, -6, -7, -8, -9};
+        int depth = 9;
         Direction clickedFace = context.getClickedFace();
 
         for (int width : iterateWidthHeight) {
             for (int height : iterateWidthHeight) {
-                for (int depth : itterateDepth) {
-                    newBlockPos = offsetBlock(world,context, newBlockPos, clickedFace,width,height, depth);
+                for (int d = 0; d <= (depth - 1); d++) {
+                    newBlockPos = offsetBlock(blockPos, clickedFace, width, height, d);
+                    BlockState newClickedBlock = world.getBlockState(newBlockPos);
+                    breakAndGivePlayerItems(newBlockPos, context);
+
                 }
             }
         }
     }
 
-    private BlockPos offsetBlock(Level world,UseOnContext context, BlockPos blockToOffset, Direction clickedDirection, int width, int height, int depth){
-        int negativeDepth;
+    private BlockPos offsetBlock(BlockPos blockToOffset, Direction clickedDirection, int width, int height, int depth) {
+        int negativeDepth = depth * -1;
         BlockPos newBlockPos = blockToOffset;
-        BlockState newClickedBlock = world.getBlockState(blockToOffset);
-        negativeDepth = depth * -1;
-
-        if(clickedDirection == Direction.SOUTH){
-            //width, height, depth
-            newBlockPos = blockToOffset.offset(width, height, negativeDepth);
+        switch (clickedDirection) {
+            case SOUTH:
+                newBlockPos = blockToOffset.offset(width, height, negativeDepth);break;
+            case NORTH:
+                newBlockPos = blockToOffset.offset(width, height, depth);break;
+            case EAST:
+                newBlockPos = blockToOffset.offset(negativeDepth, width, height);break;
+            case WEST:
+                newBlockPos = blockToOffset.offset(depth, width, height);break;
+            case UP:
+                newBlockPos = blockToOffset.offset(width, negativeDepth, height);break;
+            case DOWN:
+                newBlockPos = blockToOffset.offset(width, depth, height);break;
         }
-        else if(clickedDirection == Direction.NORTH){
-            //width, height, depth
-            newBlockPos = blockToOffset.offset(width, height, depth);
-        }
-        else if(clickedDirection == Direction.EAST){
-            //depth, width, height
-            newBlockPos = blockToOffset.offset(negativeDepth, width, height);
-        }
-        else if(clickedDirection == Direction.WEST){
-            //depth, width, height
-            newBlockPos = blockToOffset.offset(depth, width, height);
-        }
-        else if(clickedDirection == Direction.UP){
-            //width, depth, height
-            newBlockPos = blockToOffset.offset(width, negativeDepth, height);
-        }
-        else if(clickedDirection == Direction.DOWN){
-            //width, depth, height
-            newBlockPos = blockToOffset.offset(width, depth, height);
-        }
-
-        newClickedBlock = world.getBlockState(newBlockPos);
-        if(blockValidToBreak(newClickedBlock, context, world)){
-            world.destroyBlock(newBlockPos, true);
-        }
-
-        return blockToOffset;
+        return newBlockPos;
     }
 
     private boolean blockValidToBreak(BlockState clickedBlock, UseOnContext context, Level world) {
-        BlockPos blockpos = context.getClickedPos();
-        return clickedBlock.getDestroySpeed(world,context.getClickedPos()) > 0f;
+        return clickedBlock.getDestroySpeed(world, context.getClickedPos()) > 0f &&
+                isBlockSpawnProtected(context.getPlayer(),context.getClickedPos());
     }
 
+    //i took this code basically exactly from dshadowwolfs bit
+    private boolean isBlockSpawnProtected(Player player, BlockPos pos) {
+        if (ServerLifecycleHooks.getCurrentServer().isUnderSpawnProtection((ServerLevel) player.getCommandSenderWorld(), pos, player)) {
+            return false;
+        }
+        return Arrays.stream(Direction.values()).allMatch(e -> player.mayUseItemAt(pos, e, ItemStack.EMPTY));
+    }
+
+    private void breakAndGivePlayerItems( BlockPos bopo, UseOnContext context){
+        List<ItemStack> drops = new LinkedList<>();
+        Level world = context.getLevel();
+        BlockState newClickedBlock = world.getBlockState(bopo);
+        Player player = context.getPlayer();
+        if(blockValidToBreak(newClickedBlock, context, world)){
+            drops.addAll(Block.getDrops(newClickedBlock, (ServerLevel) world, bopo, world.getBlockEntity(bopo), context.getPlayer(), context.getItemInHand() ));
+            world.destroyBlock(bopo, false);
+        }
+        for (ItemStack item :drops) {
+            if (!player.getInventory().add(item)) {
+                player.drop(item, false);
+            }
+        }
+    }
 }
 
 
